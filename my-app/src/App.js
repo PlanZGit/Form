6d05f0591cc318b14b7683fef015c96cd3a3b640
Vang Xiong <vangx709@gmail.com>
2023-02-22 09:10:25 -0900

    renaming YupForm-Cypress, removed datafetching

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
-// import DataFetching from "./components/DataFetching";
+
 import YupUserForm from "./components/YupUserForm";
 
 function App() {
   return (
     <div className="App">
       <YupUserForm />
-      {/* <DataFetching /> */}
     </div>
   );
 }
